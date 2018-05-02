a5b96310805badcfa87dd085b9d4afed23d6e0c9
Arthur GASPARD <agaspard@e2r10p1.42.fr>
2018-05-02 10:38:18 +0200

    outputOK

--- config_ucss.js
+++ config_ucss.js
@@ -1,11 +1,9 @@
+//print unused rules found, together with positions in CSS file:
+
 module.exports = {
 	"result": function(result) {
-		// Do something with the result object, e.g. print every rule
-		// found, together with positions in CSS file:
 		for (var s in result.selectors) {
-		// Only unused rules:
 			if (result.selectors[s].matches_html === 0) {
-			// Print position(s), given it's only one CSS file:
 				var pos_css = result.selectors[s].pos_css;
 				var key = Object.keys(pos_css)[0];
 				console.log(s + ": " + pos_css[key]);
--- css-scraper.js
+++ css-scraper.js
@@ -7,19 +7,20 @@ var path = require('path');
 //take the url as first argument
 var url = process.argv[2];
 
-//create a 'web-files' directory and store files + define depth
+//create a 'web-files' to store datas + define depth
 var options = {
 	urls: [url],
 	directory: './web-files',
 	recursive: true,
-	maxRecursiveDepth: '1'
+	maxRecursiveDepth: '2'
 };
 
 var cssArray;
 var htmlArray;
 
+//scrape url
 scrape(options).then((result) => {
-//select css files
+//select .css files
 	fs.readdir('web-files/css', function(err, files) {
 		cssArray = files.filter(function(e) {
 			return path.extname(e).toLowerCase() === '.css'
@@ -27,7 +28,7 @@ scrape(options).then((result) => {
 		for(var i = 0; i < cssArray.length; i++) {
 			cssArray[i]="./web-files/css/"+cssArray[i];
 		}
-//select html files
+//select .html files
 		fs.readdir('web-files', function(err, files) {
 			htmlArray = files.filter(function(e) {
 				return path.extname(e).toLowerCase() === '.html'
@@ -35,7 +36,7 @@ scrape(options).then((result) => {
 			for(var i = 0; i < htmlArray.length; i++) {
 				htmlArray[i]="./web-files/"+htmlArray[i];
 			}
-//compare css and html
+//compare css and html using ucss
 			var css = cssArray;
 			var html = htmlArray;
 			var context = {
@@ -44,70 +45,10 @@ scrape(options).then((result) => {
 			var logger = null;
 			ucss.analyze(html, css, context, logger, function(result) {
 				console.log(config.result(result));
+//result goes through config_ucss.js to modify output
 			});
 		});
 	});
 }).catch((err) => {
 	console.log(err);
 });
-
-//var cssFiles;
-//var htmlFiles;
-//var html;
-//var css;
-//var context;
-//var logger;
-/*
-Promise.all([scrape(options).then(function(cssFiles) {
-	fs.readdirSync('web-files/css', function(err, files) {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		var cssFiles = files.filter(function(e) {
-			return path.extname(e).toLowerCase() === '.css'
-		});
-console.log('cssFiles = ' + cssFiles);
-	})}, errHandler).then(function(htmlFiles) {
-	fs.readdirSync('web-files', function(err, files) {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		var htmlFiles = files.filter(function(e) {
-			return path.extname(e).toLowerCase() === '.html'
-		});
-console.log('htmlFiles = ' + htmlFiles);
-	})}, errHandler)]).then(() => {
-//console.log('cssFiles 2 = ' + cssFiles);
-//console.log('htmlFiles 2 = ' + htmlFiles);
-
-		var css = [cssFiles];
-		var html = [htmlFiles];
-		var context = {
-			timeout: 400
-		};
-		var logger = null;
-
-		ucss.analyze(html, css, context, logger, function(result) {
-		console.log('CSS COMPARISON OK');
-		console.log('result = ' + result);
-		})
-	}, errHandler);
-*/
-/*
-.then((cssFiles) => {
-	var css = ["./web-files/css/css.css"];
-	var html = ["./web-files/index.html"];
-	var context = {
-		timeout: 400
-	};
-	var logger = null;
-
-console.log('cssFiles 2 = ' + cssFiles);
-	ucss.analyze(html, css, context, logger, function(result) {
-		console.log('CSS COMPARISON OK');
-		console.log('result = ' + result);
-	});
-}, errHandler);
-*/
